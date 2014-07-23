3681c37207ed8d78174c9b27c56550512567895c
ccprog <ccprog@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-07-23 14:18:51 +0000

    publishing version 0.3.0
    git-svn-id: https://plugins.svn.wordpress.org/crosswordsearch/tags/0.3.0@953505 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ app.php
@@ -0,0 +1,171 @@
+
+<?php // build mode has an action menu including a name selection for server reload
+
+if ( 'build' == $mode ) {
+
+?>
+    <div><dl class="cse menu" cse-select="command" cse-options="commandList" cse-model="entry" cse-is-menu cse-template="crw-menu" ng-init="entry='<?php _e('Riddle...', 'crw-text') ?>'"></dl></div>
+    <p class="error" ng-if="loadError">{{loadError.error}}</p>
+    <p class="error" ng-repeat="msg in loadError.debug">{{msg}}</p>
+    <p class="name">{{crosswordData.name}}</p>
+    <form name="meta">
+        <label for ="description"><?php _e('Give a hint which words should be found:', 'crw-text') ?></label>
+        <textarea ng-model="crosswordData.description" name="description" crw-add-parsers="sane"></textarea>
+        <p class="error" ng-show="meta.$error.sane"><?php _e('Dont\'t try to be clever!', 'crw-text') ?></p>
+    </form>
+    <dl class="crw-level">
+        <dt><span><?php _e('Select a difficulty level:', 'crw-text') ?></span>
+            <dl class="cse" cse-select="level" cse-options="levelList" cse-model="crosswordData.level" cse-template="crw-level-number"></dl>
+        </dt>
+<?php // single solve/preview only shows the name
+
+} else {
+
+    if ( $is_single ) {
+
+?>
+    <p class="name">{{crosswordData.name}}</p>
+<?php // multi solve has a name selection
+
+    } else {
+
+?>
+    <div><dl class="cse name" title="<?php _e('Select a riddle', 'crw-text') ?>" cse-select="load" cse-options="namesInProject" cse-model="loadedName"></dl></div>
+    <p class="error" ng-if="loadError">{{loadError.error}}</p>
+    <p class="error" ng-repeat="msg in loadError.debug">{{msg}}</p>
+
+<?php
+
+    }
+
+?>
+    <p class="crw-description" ng-show="crosswordData.description"><em><?php _e('Find these words in the riddle:', 'crw-text') ?></em> {{crosswordData.description}}</p>
+    <dl class="crw-level">
+        <dt><?php _e('Difficulty level', 'crw-text') ?> {{crosswordData.level+1}}</dt>
+<?php
+
+}
+
+?>
+        <dd><?php _e('Word directions', 'crw-text') ?>:
+            <strong ng-show="crw.getLevelRestriction('dir')"><?php _e('only to the right and down', 'crw-text') ?>,</strong>
+            <strong ng-show="!crw.getLevelRestriction('dir')"><?php _e('any, including the diagonals and backwards', 'crw-text') ?>,</strong>
+            <br /><?php _e('List of words that should be found', 'crw-text') ?>:
+            <strong ng-show="crw.getLevelRestriction('sol')"><?php _e('visible before found', 'crw-text') ?></strong>
+            <strong ng-show="!crw.getLevelRestriction('sol')"><?php _e('hidden before found', 'crw-text') ?></strong>
+        </dd>
+    </dl>
+    <div class="crw-crossword<?php echo ( 'build' == $mode ? ' wide" ng-style="styleCrossword()' : '' ) ?>" ng-controller="SizeController" ng-if="crosswordData">
+        <div ng-style="styleGridSize()" class="crw-grid<?php if ( 'build' == $mode ) echo ' divider' ?>">
+<?php // resize handles
+
+if ( 'build' == $mode ) {
+
+?>
+            <div crw-catch-mouse down="startResize" up="stopResize">
+                <div title="<?php _e('Drag to move the border of the riddle', 'crw-text') ?>" id="handle-left" transform-multi-style style-name="size-left" ng-style="modLeft.styleObject['handle-left'].style"></div>
+                <div title="<?php _e('Drag to move the border of the riddle', 'crw-text') ?>" id="handle-top" transform-multi-style style-name="size-top" ng-style="modTop.styleObject['handle-top'].style"></div>
+                <div title="<?php _e('Drag to move the border of the riddle', 'crw-text') ?>" id="handle-right" transform-multi-style style-name="size-right" ng-style="modRight.styleObject['handle-right'].style"></div>
+                <div title="<?php _e('Drag to move the border of the riddle', 'crw-text') ?>" id="handle-bottom" transform-multi-style style-name="size-bottom" ng-style="modBottom.styleObject['handle-bottom'].style"></div>
+            </div>
+<?php
+
+}
+
+?>
+        </div>
+        <div class="crw-mask" ng-style="styleGridSize()">
+<?php // crossword table
+
+if ( 'preview' == $mode ) {
+
+?>
+            <table class="crw-table" ng-style="styleShift()" ng-controller="TableController" ng-Init="setMode('<?php echo $mode ?>')">
+                <tr ng-repeat="row in crosswordData.table" crw-index-checker="line">
+                    <td class="crw-field" ng-repeat="field in row" crw-index-checker="column">
+                        <div><span>{{field.letter}}</span>
+<?php
+
+} else {
+
+?>
+            <table class="crw-table" ng-style="styleShift()" ng-controller="TableController" ng-Init="setMode('<?php echo $mode ?>')" crw-catch-mouse down="startMark" up="stopMark" prevent-default>
+                <tr ng-repeat="row in crosswordData.table" crw-index-checker="line">
+                    <td class="crw-field" ng-repeat="field in row" crw-index-checker="column">
+                        <div <?php if ( 'build' == $mode ) { echo 'ng-click="activate(line, column)"'; } ?> ng-mouseenter="intoField(line, column)" ng-mouseleave="outofField(line, column)">
+                            <button tabindex="-1" unselectable="on" ng-keydown="move($event)" ng-keypress="type($event)" crw-set-focus>{{field.letter}}</button>
+<?php
+
+}
+
+?>
+                            <div unselectable="on" ng-repeat="marker in getMarks(line, column)" class="crw-marked" ng-class ="getImgClass(marker)"></div>
+                        </div>
+                    </td>
+                </tr>
+            </table>
+        </div>
+<?php // fill/empty buttons
+
+if ( 'build' == $mode ) {
+
+?>
+        <p ng-style="styleExtras()">
+            <button class="fill" ng-click="randomize()" title="<?php _e('Fill all empty fields with random letters', 'crw-text') ?>" alt="<?php _e('Fill fields', 'crw-text') ?>"></button><button class="empty" ng-click="empty()" title="<?php _e('Empty all fields', 'crw-text') ?>" alt="<?php _e('Empty', 'crw-text') ?>"></button>
+        </p>
+<?php // controls and output area
+
+}
+
+?>
+    </div>
+    <div class="crw-controls">
+<?php // build mode: wordlist with color chooser and delete button
+
+if ( 'build' == $mode ) {
+
+?>
+        <ul class="crw-word">
+            <li ng-class="{'highlight': isHighlighted()}" ng-repeat="word in wordsToArray(crosswordData.words) | orderBy:'ID'" ng-controller="EntryController">
+                <dl class="cse crw-color" title="{{word.color}}" cse-template="color-select" cse-select="color" cse-options="colors" cse-model="word.color"></dl>
+                <span>{{word.fields | joinWord}} (<?php
+                /// translators: first two arguments are line/column numbers, third is a direction like "to the right" or "down"
+                printf( __('from line %1$s, column %2$s %3$s', 'crw-text'), '{{word.start.y + 1}}', '{{word.start.x + 1}}', '{{localizeDirection(word.direction)}}') ?>)</span>
+                <button class="trash" ng-click="deleteWord(word.ID)" title="<?php _e('Delete', 'crw-text') ?>"></button>
+            </li>
+        </ul>
+<?php // preview mode: wordlist
+
+} elseif ( 'preview' == $mode ) {
+
+?>
+        <ul class="crw-word">
+            <li ng-repeat="word in wordsToArray(crosswordData.words) | orderBy:'ID'" ng-controller="EntryController">
+                <img title="{{word.color}}" ng-src="<?php echo CRW_PLUGIN_URL ?>images/bullet-{{word.color}}.png">
+                <span>{{word.fields | joinWord}}</span>
+            </li>
+        </ul>
+<?php // solve mode: solution status and restart button, wordlist as solution display
+
+} else {
+
+?>
+        <p ng-show="crosswordData.name">
+            <span ng-if="count.solution<count.words"><?php printf( __('You have found %1$s of %2$s words', 'crw-text'), '{{count.solution}}', '{{count.words}}' ) ?></span>
+            <span ng-if="count.solution===count.words"><?php printf( __('You have found all %1$s words!', 'crw-text'), '{{count.words}}' ) ?></span>
+            <button class="restart" ng-click="restart()" ng-disabled="loadedName!=crosswordData.name" title="<?php _e('Restart solving the riddle', 'crw-text') ?>" alt="<?php _e('Restart', 'crw-text') ?>"></button>
+        </p>
+        <ul class="crw-word" ng-class="{'palid': crw.getLevelRestriction('sol')}">
+            <li ng-class="{'highlight': isHighlighted(), 'found': word.solved}" ng-repeat="word in wordsToArray(crosswordData.solution) | orderBy:'ID'" ng-controller="EntryController">
+                <img ng-if="word.solved" title="{{word.color}}" ng-src="<?php echo CRW_PLUGIN_URL ?>images/bullet-{{word.color}}.png">
+                <img ng-if="!word.solved" title="grey" ng-src="<?php echo CRW_PLUGIN_URL ?>images/bullet-grey.png">
+                <span>{{word.fields | joinWord}}</span>
+            </li>
+        </ul>
+<?php
+
+}
+
+?>
+    </div>
+    <p ng-show="crosswordData.author" class="copyright"><?php _e('Authored by', 'crw-text') ?> {{crosswordData.author}}</p>
---
+++ readme.txt
@@ -0,0 +1,167 @@
+=== crosswordsearch ===
+Contributors: ccprog
+Tags: game, crossword, shortcode, educational, multilingual
+Requires at least: 3.6
+Tested up to: 3.9.1
+Stable tag: trunk
+License: see LICENCE file
+License URI: https://github.com/ccprog/crosswordsearch/LICENCE
+
+Adds a wordsearch-style crossword with a shortcode. Users can develop new riddles, save them depending on specific rights, or solve stored riddles.
+
+== Description ==
+
+Crosswordsearch is a WordPress plugin, based on the [AngularJs](http://angularjs.org/)
+Javascript framework, for designing and playing wordsearch-style crosswords. 
+It is being developed for [RadiJojo.de](http://radijojo.de), the International Children's
+Media Network.
+
+* Page visitors may try to solve the published riddles.
+* Registered users may develop new riddles or modify existing ones depending on their rights.
+* For saving their work, they must not be logged in, but only give their user name and
+  password at the time of upload.
+* Each crossword may contain a freetext description of the task to complete and the name of a
+  copyright owner.
+* Crosswords have a difficulty level that relates to the used word directions and the
+  listing of the searched words before they have been found.
+* Crosswords are organised in projects that share a common set of editors.
+* Users may be given a *restricted editor* status, which gives them the right to upload
+  new riddles. These riddles will only become visible for other users if a *full editor*
+  approves them.
+* Default and maximum difficulty levels are set for projects.
+* Setting up projects and editors is reserved to the blog administrators.
+
+= Shortcode usage =
+
+Authors can add a shortcode tag to any page or post to add a Crossword game to that page:
+
+`[crosswordsearch mode="solve" project="MyProject"]`
+
+will add a game area for solving riddles that belong to the project `MyProject`. All saved
+and approved riddles belonging to the project can be loaded through a menu.
+
+`[crosswordsearch mode="solve" project="MyProject" name="MyFirstCrossword"]`
+
+will show only the crossword `MyFirstCrossword` for solving. No other crossword will be
+selectable.
+
+`[crosswordsearch mode="build" project="MyProject"]`
+
+will add a game area for developing new and editing existing riddles that belong to the
+project `MyProject`. All saved and approved riddles belonging to the project can be
+loaded through a menu. Initially visible will be the riddle with the alphabetically first
+name.
+
+`[crosswordsearch mode="build" project="MyProject" name="MyFirstCrossword"]`
+
+works as above, but will initially show the crossword saved under the name `MyFirstCrossword`.
+All other riddles can still be loaded through the menu.
+
+`[crosswordsearch mode="build" project="MyProject" name=""]`
+
+works as above, but will initially show an empty new crossword.
+
+`[crosswordsearch mode="build" restricted="1" project="MyProject"]`
+
+will add a game area for developing new riddles for the project `MyProject` by restricted
+editors. Restricted editors can work on a riddle as long as they stay on the page,
+but it will not be visible for anyone else. No menu for selecting other riddles is
+presented, and on page load an empty new crossword will be shown.
+
+== Installation ==
+
+1. Install from *Plugins -> Add New*
+2. Or download the .zip and extract to `wp-content/plugins`
+3. Activate the plugin from the *Plugins* menu
+4. Go to *Settings -> Crosswordsearch* for the next steps
+
+= User management =
+
+The *Options* tab is used to hook up the Worpress user roles to crossword editing rights.
+For each role you can choose between three categories:
+
+* *none* means users with that role may intereract the same way as casual page visitors.
+  They may solve published riddles, design a new riddle, or work on the modification of an
+  existing riddle, but they are not allowed to save the result of their development.
+* *restricted* means users with that role may design new riddles on pages that display
+  the app in **build** mode and with the **restricted** modifier set. They can save
+  the result of their development, but it will not appear on other pages unless they
+  are approved by a full editor.
+* *full* means users with that role may design a new riddle, or work on the modification
+  of an existing riddle. If they have been added to the editors of a **project**, they
+  can save their work.  
+  Full editors have access to the *Review riddles in projects* tab of the
+  *Settings -> Crosswordsearch* page, but they will only see riddles in projects where
+  they have been added to the editors. For those projects, they can preview riddles,
+  delete them or approve pending riddles by restricted editors.
+
+= Content management =
+
+The *Assign projects and editors* tab is used to set up projects. Each crossword belongs
+to a project, so you need at least one project before you can successfully use the
+shortcode.
+
+While the role setup gave users the *potential* right to save their crosswords, they need
+to be added to the project's *Full project editors* list to actually do so. Note that
+this holds true for all users, even administrators.
+
+*Restricted* users can save their riddles for approval in all projects. Every user with
+an appropriate role is authorized for this action, there is no per-user permission.
+
+== Frequently Asked Questions ==
+
+= Which work flows for adding new riddles are possible? =
+
+There are two possible setups for crossword development by users:
+
+1. **Restrict development to trusted users:**  
+    If you use pages with the app in **unrestricted build** mode, everyone assigned as an
+    editor for the project has full control over the appearance of every crossword.
+    This includes editing and deleting riddles by others.  
+    A possible use for that scenario may be a classroom situation: students can work on
+    riddles, but only the teacher knows the editor credentials and may approve
+    anything for saving.
+
+2. **Invite development and moderate:**  
+    If you use pages with the app in **restricted build** mode, you treat new uploads as
+    proposals. For example, you could confer to all subscribers or contributors the rights
+    of a restricted editor. It would be the responsibility of the full editors to review and
+    approve these proposals for publication.
+
+= Is it possible to limit the restricted editing right to specific users? =
+
+Restricted editing right is linked to a user role, and every user with that role has
+the right. If you are not happy with the coupling to standard Wordpress roles, you
+should consider introducing custom roles. There are plugins that deal with adding new
+roles and managing their capabilities.
+
+= Is it possible to limit the restricted editing to specific projects? =
+
+That is quite simply done by publishing a **restricted build** page only for that project.
+
+= Why is it impossible to edit riddles from the preview on the settings page? =
+
+As the development of a crossword must be considered as an activity protected by copyright,
+there should be no 'backdoor' for altering them. The preview page is meant for moderation.
+Altering the name of crosswords from that page is considered for future versions.
+
+= Is it possible to limit the editing of existing crosswords to their original authors? =
+
+That possibility is considered for future versions. The user id of the uploader is already
+saved to the database, albeit at this point this information is not used anywhere.
+
+== Screenshots ==
+
+1. The app in **solve** mode
+2. The app in **build** mode
+3. The *save* dialogue
+4. The *Assign projects and editors* tab
+
+== Changelog ==
+
+= 0.3 =
+* First public release
+
+== Upgrade Notice ==
+
+
