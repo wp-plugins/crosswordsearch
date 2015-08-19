a143250c88b804d2920c59e56d4b7e5cb8376e95
ccprog <ccprog@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-08-19 14:00:34 +0000

    v0.5.0
    git-svn-id: https://plugins.svn.wordpress.org/crosswordsearch/tags/0.5.0@1225159 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ reviewTab.php
@@ -0,0 +1,44 @@
+    <div class="crw-editors" ng-controller="ReviewController" ng-init="prepare('<?php echo wp_create_nonce( NONCE_CROSSWORD ) . "','" . wp_create_nonce( NONCE_REVIEW ); ?>')">
+        <table class="crw-options">
+            <tr>
+                <th class="project"><?php _e('Projects', 'crw-text') ?></th>
+                <th><?php _e('Approved crosswords', 'crw-text') ?></th>
+                <th class="between"></th>
+                <th><?php _e('Crosswords pending approval', 'crw-text') ?></th>
+            </tr>
+            <tr>
+                <td class="project">
+                    <select size="10" ng-model="selectedProject" ng-options="project as project.name for project in projects | orderBy:'name'"></select>
+                </td>
+                <td class="crosswordname">
+                    <select size="10" ng-model="selectedCrossword.confirmed" ng-options="name for name in selectedProject.confirmed | orderBy:'toString()'"  crw-option-click="confirmed"></select>
+                </td>
+                <td class="between aligned">
+                    <button title="<?php _e('Approve the marked crossword to be displayed for everyone', 'crw-text') ?>" ng-click="confirm()" ng-disabled="!selectedProject || !selectedProject.pending.length">&lt;</button><br />
+                </td>
+                <td class="crosswordname">
+                    <select size="10" ng-model="selectedCrossword.pending" ng-options="name for name in selectedProject.pending | orderBy:'toString()'" crw-option-click="pending"></select>
+                </td>
+            </tr>
+            <tr class="actions">
+                <td></td>
+                <td>
+                    <button title="<?php _e('Delete the selected approved crossword', 'crw-text') ?>" ng-click="deleteCrossword('confirmed')" ng-disabled="!selectedCrossword.confirmed">−</button>
+                </td>
+                <td class="between"></td>
+                <td>
+                    <button title="<?php _e('Delete the selected pending crossword', 'crw-text') ?>" ng-click="deleteCrossword('pending')" ng-disabled="!selectedCrossword.pending">−</button>
+                </td>
+            </tr>
+        </table>
+        <h3><input type="checkbox" title="<?php _e('Show a preview of the selected crossword', 'crw-text') ?>" ng-model="preview"><?php _e('Preview', 'crw-text') ?></input></h3>
+        <div ng-if="preview" class="crw-wrapper" ng-controller="CrosswordController" ng-init="commandState='preview'">
+<?php
+
+    $mode = 'preview';
+    $is_single = true;
+    include 'app.php';
+
+?>
+        </div>
+    </div>
